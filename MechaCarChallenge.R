4fd5560519f2b6d39d32c7c924e16f646a2f946d
Keerthana Jayakumar <keetu.jk@gmail.com>
2021-04-10 22:50:02 -0400

    Adding Deliverable 2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -9,3 +9,12 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 
 #Calculate summary
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = mechacar_data))
+
+#Read suspension coil data
+suspension_data <- read.csv('Suspension_coil.csv')
+
+#Create summary statistics of Suspension Data
+total_summary <-  suspension_data%>%summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD = sd(PSI), .groups = 'keep') 
+
+#Create summary statistics of Suspension Data grouped by lot
+lot_summary <- suspension_data%>%group_by(Manufacturing_Lot)%>%summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD = sd(PSI), .groups = 'keep') 
